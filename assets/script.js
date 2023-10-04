9eee758aae7711b026975eec585725c5146497bc
rpm3 <mancuso.b3@gmail.com>
2023-10-04 18:50:21 -0500

    Added question and answer variables, hidden question card

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,13 @@ var score = document.querySelector(".score");
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
 
+var questionSection = document.querySelector("#question-section")
+var question = document.querySelector("#question");
+var choice1 = document.querySelector("#choice1");
+var choice2 = document.querySelector("#choice2");
+var choice3 = document.querySelector("#choice3");
+var choice4 = document.querySelector("#choice4");
+
 var correct
 var wrong
 
@@ -9,17 +16,23 @@ var timer;
 var timerCount = 5;
 var initials = "";
 
+var questionArray = ""
+
 // Runs when the page loads to pull scoreboard from local storage
 function init() {
     getScoreboard();
 }
 
-// Runs to start the game when Start button is clicked
-// function startQuiz () {
-//     startButton.disabled = true;
-//     renderQuiz();
-//     startTimer();
-// }
+function renderNextQuestion() {
+    question.textContent = "Question 2";
+    choice1.textContent = "Answer A";
+}
+
+choice1.addEventListener("click", renderNextQuestion);
+choice2.addEventListener("click", renderNextQuestion);
+choice3.addEventListener("click", renderNextQuestion);
+choice4.addEventListener("click", renderNextQuestion);
+
 
 // Function to start the timer, subtracts 15 seconds for each incorrect answer
 function startTimer () {
@@ -34,6 +47,7 @@ function startTimer () {
             clearInterval(timer);
         }
     }, 1000)
+questionSection.classList.remove("hidden");
 }
 
 // START BUTTON
